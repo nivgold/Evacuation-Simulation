687398a724ab4481b140b1935395c3f34639d60a
Niv <nivgold@post.bgu.ac.il>
2020-11-25 18:00:13 +0200

    fixed walls forces

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from simulation import Simulation
 import matplotlib.pyplot as plt
+import numpy as np
 a = Simulation(num_individuals=1, num_steps=9000, v_des=1.5)
 
 a.run()
 
-print(a.y[:, :, 0])
+# print(a.y[:, :, 0])
 print(a.evacuation_time)
+
